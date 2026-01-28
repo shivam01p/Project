af96300dc26a1eea53d4faac2b7852a2a1eeaa75
shivam01p <shivamparmar549@gmail.com>
2026-01-29 00:02:19 +0530

    Faulty calculator is completed

--- script.js
+++ script.js
@@ -1,100 +1,20 @@
-// Creating a faulty calculator
-/*
-let a= Math.random();
+let randnum = Math.random();
 
-if(a<0.1){
+console.log(randnum);
+let num1= prompt("Enter first number");
+let ope= prompt("Enter operation");
+let num2= prompt("Enter second number");
 
-    function Sum(a,b){
-        return a-b;
-    }
+let obj = {
+    "+" : "-",
+    "*" : "+",
+    "-" : "/",
+    "/" : "**"
+}
 
-    function Sub(a,b){
-        return a/b;
-    }
-
-    function Mul(a,b){
-        return a+b;
-    }
-
-    function Div(a,b){
-        return a**b;
-    }
-
-    function Expo(a,b){
-        return a*b;
-    }
-
+if(randnum < 0.1){
+    ope = obj[ope];
+    alert(`The result is: ${eval(`${num1} ${ope} ${num2}`)}`);
 }else{
-
-    function Sum(a,b){
-        return a+b;
-    }
-
-    function Sub(a,b){
-        return a-b;
-    }
-
-    function Mul(a,b){
-        return a*b;
-    }
-
-    function Div(a,b){
-        return a/b;
-    }
-
-    function Expo(a,b){
-        return a**b;
-    }
-
-}
-
-console.log("Sum=", Sum(9,3));
-console.log("Sub=", Sub(9,3));
-console.log("Mul=", Mul(9,3));
-console.log("Div=", Div(9,3));
-console.log("Expo=", Expo(9,3));
-
-*/
-
-function Sum(a,b){
-        return a+b;
-    }
-
-function Sub(a,b){
-    return a-b;
-}
-
-function Mul(a,b){
-    return a*b;
-}
-
-function Div(a,b){
-    return a/b;
-}
-
-function Expo(a,b){
-    return a**b;
-}
-
-let a= 4;
-let b= 2;
-
-let randNum = Math.random();
-
-if(randNum<0.1){
-    
-    console.log("Addition is: ", Sub(a,b));
-    console.log("Subtract is: ", Div(a,b));
-    console.log("Multiplication is: ", Sum(a,b));
-    console.log("Division is: ", Expo(a,b));
-    console.log("Exponentiation is: ", Mul(a,b));
-
-}else{
-
-    console.log("Addition is: ", Sum(a,b));
-    console.log("Subtract is: ", Sub(a,b));
-    console.log("Multiplication is: ", Mul(a,b));
-    console.log("Division is: ", Div(a,b));
-    console.log("Exponentiation is: ", Expo(a,b));
-
+    alert(`The result is: ${eval(`${num1} ${ope} ${num2}`)}`);
 }
